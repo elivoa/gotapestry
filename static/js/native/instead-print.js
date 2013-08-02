4a380536e848d4658812bea3b23a33c08b32afa4
Elivoa <elivoa@gmail.com>
2013-08-02 19:05:02 +0800

    syd: pritn bug fix

--- static/js/native/instead-print.js
+++ static/js/native/instead-print.js
@@ -23,7 +23,7 @@ function printOrder(){
 	LODOP=getLodop(document.getElementById('LODOP_OB'),document.getElementById('LODOP_EM'));  
 	LODOP.PRINT_INIT("代发商品清单打印");
 	LODOP.SET_PRINT_PAGESIZE(0,2410,1400,"代发商品清单打印");	 
-	var strStyleCSS = "<link href='../../css/module/print_instead_order.css' type='text/css' rel='stylesheet'>";
+	var strStyleCSS = "<link href='/static/css/module/print_instead_order.css' type='text/css' rel='stylesheet'>";
 	var orderContent = strStyleCSS + "<body>"+document.getElementById("instead").innerHTML+"</body>";
 	LODOP.ADD_PRINT_TABLE("0","3mm","RightMargin:3mm","98mm",orderContent);
 	LODOP.ADD_PRINT_TEXT("126mm","2mm","190mm","5mm","第#页/共&页");
