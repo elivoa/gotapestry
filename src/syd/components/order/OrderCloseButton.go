44d5f34501ad117f1c66bbe47eca2cdea3cab973
elivoa <elivoa@gmail.com>
2015-06-18 12:12:50 +0800

    ...

--- src/syd/components/order/OrderCloseButton.go
+++ src/syd/components/order/OrderCloseButton.go
@@ -1,46 +1,43 @@
 package order
 
 import (
-	"fmt"
 	"github.com/elivoa/got/core"
 	"syd/model"
-	"syd/service"
-	"syd/service/orderservice"
 )
 
 type OrderCloseButton struct {
 	core.Component
+	Order *model.Order
 
-	TrackNumber int64
-	Order       *model.Order
-	Customer    *model.Person
-	Referer     string
+	// TrackNumber int64
+	// Customer    *model.Person
+	// Referer     string
 }
 
 func (p *OrderCloseButton) Setup() {
-	if p.Order == nil {
-		order, err := orderservice.GetOrderByTrackingNumber(p.TrackNumber)
-		if err != nil {
-			panic(err)
-		}
-		p.Order = order
-	} else {
-		// set customer if nil
-		if p.Customer == nil && p.Order.Customer != nil {
-			p.Customer = p.Order.Customer
-		}
-		p.TrackNumber = p.Order.TrackNumber
-	}
+	// if p.Order == nil {
+	// 	order, err := orderservice.GetOrderByTrackingNumber(p.TrackNumber)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	p.Order = order
+	// } else {
+	// 	// set customer if nil
+	// 	if p.Customer == nil && p.Order.Customer != nil {
+	// 		p.Customer = p.Order.Customer
+	// 	}
+	// 	p.TrackNumber = p.Order.TrackNumber
+	// }
 
-	if p.Customer == nil {
+	// if p.Customer == nil {
 
-		person, err := service.Person.GetPersonById(p.Order.CustomerId)
-		if err != nil {
-			panic(err)
-		}
-		if person == nil {
-			panic(fmt.Sprintf("Customer not found, id: %v", p.Order.CustomerId))
-		}
-		p.Customer = person
-	}
+	// 	person, err := service.Person.GetPersonById(p.Order.CustomerId)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	if person == nil {
+	// 		panic(fmt.Sprintf("Customer not found, id: %v", p.Order.CustomerId))
+	// 	}
+	// 	p.Customer = person
+	// }
 }
